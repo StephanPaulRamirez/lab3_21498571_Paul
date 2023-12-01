76dcc815738646ab8033981ab396bcf5690d6d57
StephanPaulRamirez <123525842+StephanPaulRamirez@users.noreply.github.com>
2023-12-01 01:33:54 -0300

    Delete codigofuente_21498571_PaulRamirez/interfaces_21498571_PaulRamirez directory

--- codigofuente_21498571_PaulRamirez/interfaces_21498571_PaulRamirez/IUsuario_21498571_PaulRamirez.java
+++
@@ -1,7 +0,0 @@
-package codigofuente_21498571_PaulRamirez.Interfaces_21498571_PaulRamirez;
-
-public interface IUsuario_21498571_PaulRamirez {
-    String toString();
-    boolean equals(Object u);
-    String getUserName();
-}
